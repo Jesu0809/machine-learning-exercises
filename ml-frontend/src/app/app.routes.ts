9ef8f7031bd81a1da139a32f1c3c143021d16a0d
Jesu0809 <jesusnuncira15@gmail.com>
2026-08-29 10:46:39 -0500

    Add linear regression calculator' (#4)
    * Title Updated
    
    * add linear regression calculator

--- ml-frontend/src/app/app.routes.ts
+++ ml-frontend/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { MlInfo } from './pages/ml-info/ml-info';
+import { LinearRegression } from './pages/linear-regression/linear-regression';
 
 export const routes: Routes = [
-  { path: '', component: MlInfo },
+  { path: '', redirectTo: 'ml-info', pathMatch: 'full' },
+  { path: 'ml-info', component: MlInfo },
+  { path: 'linear-regression', component: LinearRegression },
 ];
